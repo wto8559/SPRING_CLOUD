c655cf851e6c7dbf1c431fd29e551a14190452c0
hanghang <wto8559@163.com>
2021-02-09 17:04:50 +0800

    config 配置中心

--- cloud-config-client-3377/src/main/java/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3377/src/main/java/springcloud/controller/ConfigClientController.java
@@ -1,22 +1,23 @@
-package com.springcloud.controller;
+package springcloud.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 @RefreshScope
 public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
 
     @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/configInfo")
-    public String getConfigInfo(){
-        return configInfo;
+    public String configInfo(){
+        return "serverPort: " +serverPort+ "\t\n\n configInfo: "+ configInfo;
     }
 
 }
